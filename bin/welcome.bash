f3cba7b7ddda2bc6103fbbb0df4fec43987d7b45
Thomas Freedman <thomasfreedman@protonmail.com>
2021-09-23 14:56:59 -0500

    Add files via upload
    Updated logic for default hotspot.

--- bin/welcome.bash
+++ bin/welcome.bash
@@ -1,6 +1,25 @@
 #!/bin/bash
+www=/var/www/html
+apd=hotspot/config/hostapd.conf
 
 cd /home/ipfs/bin
+
+# This is a one-time setup for a randomized SSID for Access Point
+grep "PirateBox____" $apd > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  newHSssid=PirateBox$((1 + $RANDOM % 9999))
+  sed -i -e "/^ssid=/c\ssid=$newHSssid" $apd
+  sed -i -e "/^wpa_passphrase=/c\wpa_passphrase=@RRRsp0t" $apd
+  sudo cp $apd /etc/hostapd/.
+
+  # If PBIP8 is installed, update docs under web server
+  if [ -f ${www}/pbox-ssid.html ]; then
+    sed -i "s/inactive/$newHSssid/" ${www}/pbox-ssid.html
+    h="href='pbox-ssid.html'"
+    t="title='SSID: "$newHSssid"'"
+    sed -i "s/<\!--ssid-->/<a $h $t>Pirate Box Hotspot<\/a>/" ${www}/index.html
+  fi
+fi
 
 if [ -f welcome.off ]; then exit; fi
 
