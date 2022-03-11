a9baa15ea8c787f14007fa0a589f12d802ef49cf
Thomas Freedman <thomasfreedman@protonmail.com>
2022-03-10 20:33:58 -0500

    Update pbvg.bash
    Fixed problem, improved UX. Same as Pirate Stick now, except uses lxterminal instead of xfce4-terminal on line 52

--- bin/videoGrabber/pbvg.bash
+++ bin/videoGrabber/pbvg.bash
@@ -8,11 +8,24 @@ SQLDB=$BASE/pbvg.sqlite
 intro() {
   cat <<'EOI'
 This tool will ask you for a list of one or more URLs to grab
-media content from and use that input to run the Pirate Box
-Video Grabber (PBVG) tool, which will scrape the URL(s), add
-the media to IPFS and add the metadata for it to a SQLite
-database used by the Pirate Box Search Tool.
+media content from and use that input to run the Pirate Stick
+Video Grabber (PBVG) tool.
+
+The tool will scrape the URL(s), add the content and metadata
+for it to IPFS and record that in a SQLite database for use by
+the Pirate Stick Search Tool to make it easy to find and filter.
 EOI
+}
+
+makeBatch() {
+  CFG=$($ADD_URLS "$BASE" "$URLS")  # Returns the config file with URLs added
+  cat << EOF > /tmp/psvg.bash
+#!/bin/bash
+$GRABBER -c $CFG -d $SQLDB 
+echo "Process complete. Press any key to close this window."
+read
+EOF
+  chmod 755 /tmp/pbvg.bash
 }
 
 # Inform user
@@ -20,16 +33,13 @@ zenity --info --text="$(intro)" --width=475 --height=100
 
 TITLE='URL List Entry'
 MSG='Please enter your comma separated list of URLs'
-URLS=`zenity --entry --title="$TITLE" --text="$MSG" --width=400 --height=50`
-
-pushd $BASE > /dev/null 2>&1
-CFG=$($ADD_URLS "$BASE" "$URLS")  # Returns the config file with URLs added
+URLS=$(zenity --entry --title="$TITLE" --text="$MSG" --width=400)
 
 if [ $? -eq 0 ]; then
   # Make sure we're still online with Internet
   $(ping -c 1 1.1.1.1 > /dev/null 2>&1)
   if [ $? -ne 0 ]; then
-    zenity --error --text="You're not online!" --width=250 --height=100
+    zenity --error --text="You're not online!" --width=250
     exit 1
   fi
   # Make sure the local IPFS node is running
@@ -37,21 +47,12 @@ if [ $? -eq 0 ]; then
   if [ "$AOK" == "" ]; then
     zenity --error --text="IPFS is offline!" --width=250 --height=100
   else
-    # All systems go, run the grabber. Too bad lxterminal can't be positioned!
-    #
-    # Too bad xterm & lxterm doesn't allow copy to clipboard!
-    # lxterm -geometry 80x20+150+100 -fa 'Monospace' -fs 12 -title "$TITLE"  -e "$CMD"
-    CMD="$GRABBER -c $CFG -d $SQLDB; bash"
-    TITLE="Pirate Box Video Grabber"
-    echo "lxterminal -geometry=80x20 -t $TITLE -e $CMD"
-    lxterminal --geometry=80x20 -t "$TITLE"  -e "$CMD"
-    #$($GRABBER -c $CFG -d $SQLDB >> pbvg.log 2>&1)
+    makeBatch
+    TITLE="Pirate Stick Video Grabber"
+    lxterminal --geometry=80x20 -t "$TITLE" -e /tmp/pbvg.bash
   fi
 else
-  MSG="Problem with your list of URLs!\nthey must all start with 'http'"
+  MSG="Problem with your list of URLs!\nThe first 4 letters must be 'http'"
   zenity --error --text="$MSG" --width=250 --height=100
 fi
-popd > /dev/null 2>&1
-
-
 
